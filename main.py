4635a896068f4ee26cd011cc8fa87d8cdd827bea
Gustavo Amaral <gugamaral@gmail.com>
2024-05-22 21:43:17 -0300

    Versão 1.1

--- main.py
+++ main.py
@@ -16,48 +16,44 @@ while True:
         case 1:
             print(f'Filme escolhido por {nome}: Titanic.')
             if idade < 12:
-                print('Proibída a entrada')
-            elif idade >= 12:
-                print('Bom Filme')
+                print('Proibida a entrada')
             else:
-                continue
+                print('Bom Filme')
         case 2:
             print(f'Filme escolhido por {nome}: Vivendo a Vida Adoidado.')
             if idade < 16:
-                print('Proibída a entrada')
-            elif idade >= 16:
-                print('Bom Filme')
+                print('Proibida a entrada')
             else:
-                continue
+                print('Bom Filme')
         case 3:
             print(f'Filme escolhido por {nome}: Forrest Gump.')
             if idade < 18:
-                print('Proibída a entrada')
-            elif idade >= 18:
-                print('Bom Filme')
+                print('Proibida a entrada')
             else:
-                continue
+                print('Bom Filme')
         case 4:
             print(f'Filme escolhido por {nome}: O Resgate do Soldado Ryan.')
             if idade < 21:
-                print('Proibída a entrada')
-            elif idade >= 21:
-                print('Bom Filme')
+                print('Proibida a entrada')
             else:
-                continue
+                print('Bom Filme')
         case 5:
             print(f'Filme escolhido por {nome}: De Volta Para o Futuro 3.')
             if idade < 10:
-                print('Proibída a entrada')
-            elif idade >= 10:
-                print('Bom Filme')
+                print('Proibida a entrada')
             else:
-                continue
+                print('Bom Filme')
         case _:
             print('Filme inexistente.')
 
 
+    continuar = input('Escolha outro filme ')
+     
 
-
-
+    if continuar == 's':
+        continue
+    elif continuar == 'n':
+        break
+    else:
+        print('Opção inválida')
 
